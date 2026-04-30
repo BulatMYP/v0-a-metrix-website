aecd87df7e27110fa9eefd1c54443c57e15f8eca
BulatMYP <muhtasip@mail.ru>
2026-04-30 19:30:45 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+ // output: 'export',
   basePath: '',          // <- добавьте эту строку
   assetPrefix: '',       // <- и эту
   images: {
