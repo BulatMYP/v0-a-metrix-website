9f079778d4480e629e0af552bbd83872ed844765
BulatMYP <muhtasip@mail.ru>
2026-05-02 13:32:23 +0300

    Delete app/api directory

--- app/api/payment/cancel/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PaymentCancelPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold text-red-600">Оплата отменена</h1>
-      <p className="mt-2">Вы можете попробовать снова или связаться с нами.</p>
-    </div>
-  );
-}
--- app/api/payment/success/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PaymentSuccessPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold text-green-600">Оплата успешно выполнена!</h1>
-      <p className="mt-2">Спасибо за покупку! Мы свяжемся с вами в ближайшее время.</p>
-    </div>
-  );
-}
